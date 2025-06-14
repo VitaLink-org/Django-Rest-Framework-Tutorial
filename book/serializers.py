be4879d9fd96461c73ab83f99f1ca9fac90c354f
Kotaro0722 <kotaro.tanaka@4nonome.com>
2025-06-15 06:39:43 +0900

    バリデーションを追加

--- book/serializers.py
+++ book/serializers.py
@@ -1,4 +1,6 @@
+from django.core.validators import RegexValidator
 from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
 
 from .models import Book
 
@@ -13,6 +15,37 @@ class BookSerializers(serializers.ModelSerializer):
 
         # 利用しないフィールドを指定
         # excludes = ["created_at"]
+
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Book.objects.all(),
+                fields=("title", "price"),
+                message="タイトルと価格でユニークになっていなければいけません",
+            )
+        ]
+        extra_kwargs = {
+            "title": {
+                "validators": [
+                    RegexValidator(r"^D.+$", message="タイトルは「D」で始めてください")
+                ]
+            }
+        }
+
+    def validate_title(self, value):
+        # タイトルに対するバリデーション
+        if "Java" in value:
+            raise serializers.ValidationError(
+                "タイトルには「Java」を含めないでください"
+            )
+        return value
+
+    def validate(self, data):
+        # 複数フィールド間のバリデーションメソッド
+        title = data.get("title")
+        price = data.get("price")
+        if title and "薄い本" in title and price and price > 3000:
+            raise serializers.ValidationError("薄い本は3000円を超えてはいけません")
+        return data
 
 
 class BookListSerializers(serializers.ListSerializer):
