16058343a1d88acaee008c29de51e430a8a62892
Kotaro0722 <kotaro.tanaka@4nonome.com>
2025-06-15 06:09:12 +0900

    Publisherモデルの作成・Bookモデルの修正

--- book/models.py
+++ book/models.py
@@ -5,6 +5,24 @@ from django.db import models
 # Create your models here.
 
 
+class Publisher(models.Model):
+    # 出版社モデル=1つの出版社に対して複数の本がある(多対一)
+    class Meta:
+        # テーブル名を publisher に設定
+        db_table = "publisher"
+        # 管理画面での表示名を 出版社に設定
+        verbose_name = verbose_name_plural = "出版社"
+
+    # このテーブルの主キーを設定
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    # 二つ目のフィールドを文字列型で作成【カラム名：出版社名　格納できる最大文字列：20】
+    name = models.CharField(verbose_name="出版社名", max_length=20)
+
+    # 三つ目のフィールドを日時型(datetime型)で作成【カラム名：登録日時　データが作成されたときの日時が自動入力】
+    created_at = models.DateTimeField(verbose_name="登録日時", auto_now_add=True)
+
+
 class Book(models.Model):
     # 本モデル
     class Meta:
@@ -24,7 +42,10 @@ class Book(models.Model):
     # 三つ目のフィールドを整数型(int型)で作成【カラム名：価格　値がnullや空白を許容する】
     price = models.IntegerField(verbose_name="価格", null=True, blank=True)
 
-    # 四つ目のフィールドを日時型(datetime型)で作成【カラム名：登録日時　データが作成されたときの日時が自動入力】
+    # Publisherモデルとの多対一のリレーションを作成【カラム名：出版社　親テーブル(Publisherモデル)の該当データが削除されたときに、このデータも削除】
+    publisher = models.ForeignKey(Publisher, verbose_name="出版社", on_delete=True)
+
+    # 五つ目のフィールドを日時型(datetime型)で作成【カラム名：登録日時　データが作成されたときの日時が自動入力】
     created_at = models.DateTimeField(verbose_name="登録日時", auto_now_add=True)
 
     def __str__(self):
